59f22ffebb5f45537126da101781de44c6703620
selim2066 <121637755+selim2066@users.noreply.github.com>
2025-06-09 12:48:15 +0600

    onclick done

---
+++ script.js
@@ -0,0 +1,21 @@
+function night(){
+    document.body.style.backgroundColor='black'
+     document.body.style.color='red'
+      document.body.style.fontSize='26px'
+}
+
+const day = document.getElementById('day')
+day.onclick=white
+function white()
+{
+    document.body.style.backgroundColor='white'
+    document.body.style.fontSize='16px'
+    document.body.style.color='black'
+}
+
+const julyy=document.getElementById('july')
+julyy.onclick= function (){
+    document.body.style.backgroundColor='red'
+    document.body.style.fontSize='36px'
+     document.body.style.color='white'
+}
